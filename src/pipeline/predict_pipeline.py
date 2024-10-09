1e2a4e21c058da0cb3953b8f333a79db9306c171
sahanakommalapati1009 <sahanakommalapati1009@gmail.com>
2024-10-09 18:45:12 -0400

    builiding application using flask

---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,68 @@
+# Now we will try to create a web application which willmm be interacting with this type of pickle files that we are created wrt any input data that we give
+# In web application we will be having a form where we will give all our input data that is required for predicting the student performance and then submit
+# internally in the backend we will capture the data and that data needs to probably interact with this preprocessor.pkl file and model.pkl then we are able to get the prediction or not
+import sys
+import pandas as pd
+from src.exception import CustomException
+from src.utils import load_object
+
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+    def predict(self, features):
+        try:
+            model_path='artifacts\model.pkl'
+            preprocessor_path='artifacts\preprocessor.pkl'
+            model=load_object(file_path=model_path)
+            preprocessor=load_object(file_path=preprocessor_path)
+            data_scaled=preprocessor.transform(features)
+            preds=model.predict(data_scaled)
+            return preds
+        except Exception as e:
+            raise CustomExcepttion(e,sys)
+
+# This custom class will be responsible in mapping all the inputs that we are giving in te html to the backend with this particular values
+
+class CustomData:
+    def __init__(self,
+        gender: str,
+        race_ethnicity: str,
+        parental_level_of_education,
+        lunch: str,
+        test_preparation_course: str,
+        reading_score: int,
+        writing_score: int):
+        self.gender = gender
+
+        self.race_ethnicity = race_ethnicity
+
+        self.parental_level_of_education = parental_level_of_education
+
+        self.lunch = lunch
+
+        self.test_preparation_course = test_preparation_course
+
+        self.reading_score = reading_score
+
+        self.writing_score = writing_score
+
+    # This function will return all our input in the form of a dataframe
+    # In short what wil happen is from my website whatever inputs i am probably giving will get mapped with these particular value
+    def get_data_as_data_frame(self):
+        try:
+            custom_data_input_dict = {
+                "gender": [self.gender],
+                "race_ethnicity": [self.race_ethnicity],
+                "parental_level_of_education": [self.parental_level_of_education],
+                "lunch": [self.lunch],
+                "test_preparation_course": [self.test_preparation_course],
+                "reading_score": [self.reading_score],
+                "writing_score": [self.writing_score],
+            }
+
+            return pd.DataFrame(custom_data_input_dict)
+
+        except Exception as e:
+            raise CustomException(e, sys)
